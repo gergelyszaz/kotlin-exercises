14540c4c16395438e73e07b7c6298a7108c8eaab
Gergely Száz <gergely.szaz@liferay.com>
2019-05-02 18:00:41 +0200

    #1 sort

--- 02/02.kts
+++ 02/02.kts
@@ -1,28 +1,29 @@
 println("hello")
 
-open class Fruit constructor(_weight: Int, _ripened: Boolean){
-
-    val weight: Int
-    val ripened: Boolean
-
-    init {
-        weight = _weight
-        ripened = _ripened
-    }
-
-    fun getWeight(): Int {return weight} 
-    fun isRipened(): Boolean {return ripened}
+open class Fruit constructor(val _weight: Int, val _ripened: Boolean){
+    fun isRipened(): Boolean {return _ripened}
 }
 
-class Banana constructor(_weight: Int, _isRipened: Boolean, _isCookingBanana: Boolean): Fruit(_weight, _isRipened) {
-    fun isCookingBanana() {}
+class Banana constructor(_weight: Int, _isRipened: Boolean, val _isCookingBanana: Boolean): Fruit(_weight, _isRipened) {
+    fun isCookingBanana() : Boolean { return _isCookingBanana}
 }
 
-class Cherry constructor(_weight: Int, _isRipened: Boolean, _isSourCherry: Boolean) : Fruit(_weight, _isRipened) {
-    fun isSourCherry() {}
+class Cherry constructor(_weight: Int, _isRipened: Boolean, val _isSourCherry: Boolean) : Fruit(_weight, _isRipened) {
+    fun isSourCherry() : Boolean { return _isSourCherry}
 }
 
-fun sort(fruits: List<Fruit>) {
 
+fun sort(fruits: List<Fruit>) : List<Fruit> {
+    return fruits.sortedWith( Comparator<Fruit>{ x, y ->  
+        when {
+           x.isRipened() -> -1
+           y.isRipened() -> 1
+           else -> -1
+        }
+    })
 }
+
+
+val fruits = listOf(Banana(6,false,true),Cherry(4,true,true))
+println(sort(fruits))
 
